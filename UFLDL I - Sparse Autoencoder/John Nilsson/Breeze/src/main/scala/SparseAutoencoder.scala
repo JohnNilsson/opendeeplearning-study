f1b66ed5aca0c24a84c6e609bbc42ab3b3cab558
John Nilsson <john@milsson.nu>
2013-07-01 23:03:53 +0200

    Sprinkle some comments in there

--- UFLDL I - Sparse Autoencoder/John Nilsson/Breeze/src/main/scala/SparseAutoencoder.scala
+++ UFLDL I - Sparse Autoencoder/John Nilsson/Breeze/src/main/scala/SparseAutoencoder.scala
@@ -10,18 +10,34 @@ import breeze.plot._
 
 object IMAGES {
 
+  /**
+   * Load IMAGES.mat using jmatio.
+   */
   def load() = new MatFileReader("IMAGES.mat")
     .getMLArray("IMAGES")
+    // jmatio provides some matlab like types to represent matrixes
+    // and vecotors
     .asInstanceOf[MLDouble]
+    // but we just want the underlying data to pass on
+    // to breeze. getRealByteBuffer() provides direct access to the
+    // memory mapped data.
     .getRealByteBuffer()
     .asDoubleBuffer()
 
+  /**
+   * Get one of the images in IMAGES.mat
+   */
   def getImage(img: Int) = {
     val size = 512 * 512
+
+    // Breezs DenseMatrix wants an Array as backing source.
+    // The DoubleBuffer.get should provide the quickest way to copy
+    // the data into an array.
     val array = new Array[Double](size)
     var images = load()
     images.position(img * size)
     images.get(array, 0, size)
+
     DenseMatrix.create(512, 512, array)
   }
 
